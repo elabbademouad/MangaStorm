12612d9b8595a1f475dda0e16f9def8f0fd006da
Mouad El abbade <elabbademouad@gmail.com>
2019-01-10 15:09:16 +0100

    create IRepository and RepositoryBase Infrastructure layer

---
+++ 03-Infrastructure/Infrastructure/RepositoryBase.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure
+{
+    public class RepositoryBase
+    {
+        public readonly IMongoDatabase DbContext;
+        public RepositoryBase(IMongoDatabase dbContext)
+        {
+            DbContext = dbContext;
+        }
+    }
+}
